3454cdc70c9cb17b332d8cda4a9413986606db69
joinThe <stadnikgleb@gmail.com>
2019-05-27 20:31:45 +0300

    Add project files.

---
+++ lesson4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4_2
+{
+    class Program
+    {
+
+        private static void task2(int n)
+        {
+         
+            Console.WriteLine($"All dividers of {n}");
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            task2(26);
+
+        }
+    }
+}
